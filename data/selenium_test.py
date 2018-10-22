756daf3721bb481a90420ecc63c2a6d5510c57a5
Rajesh Pujara <13rp42@caslab.queensu.ca>
2018-10-22 18:16:03 -0400

    added a couple things

--- data/selenium_test.py
+++ data/selenium_test.py
@@ -5,6 +5,9 @@ driver = webdriver.Chrome("C:/Users/rajes/OneDrive/Documents/ELEC498/ELEC-498-Ca
 
 driver.set_page_load_timeout(10)
 driver.get('http://climate.weather.gc.ca/radar/index_e.html')
-drpdwn = driver.find_element_by_xpath('//select[@name="site"]/optgroup[2]/option[12]').click()
+driver.find_element_by_xpath('//select[@name="site"]/optgroup[2]/option[12]').click()
+driver.find_element_by_xpath('//select[@name="year"]/option[1]').click()
+driver.find_element_by_id("Submit2").click()
+driver.back()
 time.sleep(4)
 driver.quit()
