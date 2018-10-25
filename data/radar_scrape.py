b30141fb7bb3c2897b3349477d8b16a3bdeba867
Rajesh Pujara <13rp42@caslab.queensu.ca>
2018-10-25 17:15:25 -0400

    fixed merged scraping code

--- data/radar_scrape.py
+++ data/radar_scrape.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import time
-from scrapetest import getgif
-import urllib
+import urllib.request
 import os
 
 driver = webdriver.Chrome("C:/Users/rajes/OneDrive/Documents/ELEC498/ELEC-498-Capstone/data/drivers/chromedriver.exe")
