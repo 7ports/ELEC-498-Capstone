c724fd750f6dafaf941c17cd866ebd163e2fa868
Evan Bonneau <bonneauevan@gmail.com>
2018-10-26 12:20:36 -0400

    adding check for directory

--- ImgSort/sortyBOI.sh
+++ ImgSort/sortyBOI.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
 
-for i in ./$1/*.gif;
+for entry in ./$1/*.gif; # cycle through all *.gif (images) entries in directory
 do
-	echo $i
-	FILE=$(basename $i)
-	echo $FILE
+	FILE=$(basename $entry) # takes file name only from path
 	INFO=(${FILE//-/ })
-	echo ${INFO[0]}
-	echo ${INFO[1]}
+	#-------------------------------------------------------------
+	# INFO is an array with the file name split with delimiter "-"
+	# INFO contents:
+	# INFO[0] = Weather Station Name
+	# INFO[1] = Year
+	# INFO[2] = Day
+	# INFO[3] = Month
+	# INFO[4] = Image No.
+	#-------------------------------------------------------------
+
+	if [ ! -d ./${INFO[0]}/${INFO[1]}/${INFO[3]}/${INFO[2]}/ ]
+	then
+		echo "the folder doesnt exist"
+		echo "./${INFO[0]}/${INFO[1]}/${INFO[3]}/${INFO[2]}/ "
+	fi
+
+	echo "----------------------------"
+	echo "MOVING FILE:" 
+	echo $FILE
+	
 done
 
 echo "exit Loop"
