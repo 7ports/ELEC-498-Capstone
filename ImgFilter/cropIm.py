a29487a6a2d10620917034a2d4bbc636cf819855
Evan <bonneauevan@gmail.com>
2018-11-18 16:47:22 -0500

    adding bash script to run python script

--- ImgFilter/cropIm.py
+++ ImgFilter/cropIm.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2 as cv
+import sys
 
+path = sys.argv[1]
+print(path)
 
-img = cv.imread('hollyrood.png', 1) 
+img = cv.imread(path, 1) 
 cv.imshow('image', img)
 cv.waitKey(0)
 
---
+++ ImgFilter/filter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python cropIm.py ./tmp/hollyrood.png 
