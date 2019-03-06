944af283718bea91b4da23a4b86f771409856d38
Rajesh Pujara <13rp42@caslab.queensu.ca>
2019-03-06 12:16:53 -0500

    finally back up to date

--- data/dataRead.py
+++ data/dataRead.py
@@ -7,7 +7,7 @@ from numpy import array, hstack, vstack
 
 
 
-os.chdir('C:/Users/rajes/OneDrive/Documents/ELEC498 numba 2/ELEC-498-Capstone/data')
+os.chdir('C:/Users/rajes/OneDrive/Documents/ELEC498numba2/ELEC-498-Capstone/data')
 
 #set up all values so they can be altered easily
 year = 2008
@@ -19,9 +19,9 @@ filename = str(year) + '-' + str(months[currmonth]) + '-' + str(day) + '-' + str
          
 
 
-f = h5py.File('images2008.hdf5', 'r')
+f = h5py.File('thiccBOI.hdf5', 'r')
 #example access of h5 file
-#print(list(f['2008']['April']['1']['0']['2008-April-1-0']))
+#list(f['2008']['April']['1']['0']['2008-April-1-0'])
 
 yvals = {}
 
@@ -35,11 +35,11 @@ for i in range(12):
 for i in range(1,28):
     for key in yvals.keys():
         for keyt in yvals[key].keys():
-            yvals[key][keyt][str(i)] = 0
+            yvals[key][keyt][str(i)] = 'N'
 
 flag = 0
 
-with open ('Master_numpy_withNULLS.csv', 'r') as csvfile:
+with open ('Master_numpy_withnoNulls_FIXED.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     
     for row in reader:
@@ -47,18 +47,18 @@ with open ('Master_numpy_withNULLS.csv', 'r') as csvfile:
         if flag == 0:
             flag = 1
             continue
-        #skip null values
-        if row[5] == 'Null':
-            continue
         yvals[row[1]][months[int(row[2]) - 1]][row[3]] = int(row[5])
 
 
-print(yvals)
+
+
 
 
 #initialize data in correct shape
 temp = np.zeros((24,31,31,3))
 #init final list to store arrays in
+npfinalx = np.array([])
+npfinaly = np.array([])
 finalx = []
 finaly = []
 
@@ -68,35 +68,51 @@ for year in range(2008, 2017):
     for currmonth in range(len(months)):
         #iterate over days
         for day in range(1,28):
-            temp = np.zeros((24,31,31,3))
-            try:
-                finaly.append(yvals[str(year)][months[currmonth]][str(day)])
-            except:
-                print("rainfall data not found, skipping")
-                continue
-            #iterate over hours
-            for hour in range(24):
-                #set proper filename variable
-                filename = str(year) + '-' + str(months[currmonth]) + '-' + str(day) + '-' + str(hour)
-                #iterate over rows
-                for i in range(31):
-                    #attempt to extract row of the image for the current hour
-                    try:
-                        temprow = list(f[str(year)][months[currmonth]][str(day)][str(hour)][filename][i])
-                    except:
-                        #if the hour cannot be found then break
-                        print("hour " + str(hour) + " for " + filename +  " not found, skipping")
-                        break
-                    #iterate over columns
-                    for k in range(31):
-                        #access a single one of those pixels
-                        temppixel = temprow[k]
-                        #iterate over RGB
-                        for z in range(3):
-                            temp[hour][i][k][z] = temppixel[z]
-            #add to the final array
-            #access y value from dictionary
-            finalx.append(temp)
-            
+            for compass in range(4):
+                temp = np.zeros((24,31,31,3))
+                try:
+                    #append to preliminary list
+                    finaly.append(yvals[str(year)][months[currmonth]][str(day)])
+                except:
+                    print("rainfall data not found, skipping")
+                    continue
+                #iterate over hours
+                for hour in range(24):
+                    #set proper filename variable
+                    filename = str(year) + '-' + str(months[currmonth]) + '-' + str(day) + '-' + str(hour) + '-' + str(compass)
+                    #iterate over rows
+                    for i in range(31):
+                        #attempt to extract row of the image for the current hour
+                        try:
+                            temprow = list(f[str(year)][months[currmonth]][str(day)][str(hour)][filename][i])
+                        except:
+                            #if the hour cannot be found then break
+                            print("hour " + str(hour) + " for " + filename +  " not found, skipping")
+                            break
+                        #iterate over columns
+                        for k in range(31):
+                            #access a single one of those pixels
+                            temppixel = temprow[k]
+                            #iterate over RGB
+                            for z in range(3):
+                                temp[hour][i][k][z] = temppixel[z]
+                #add to preliminary list of x values
+                finalx.append(temp)
 
-print(finaly)
+
+#remove all the null values from the lists
+for i in reversed(range(len(finaly))):
+    if finaly[i] == 'N':
+        del finaly[i]
+        del finalx[i]
+
+#convert the lists into arrays for storage
+npfinaly = np.asarray(finaly)
+npfinalx = np.asarray(finalx)
+
+
+#write data to file
+h5f = h5py.File('cleandata.h5', 'w')
+h5f.create_dataset('dataset_x', data = npfinalx)
+h5f.create_dataset('dataset_y', data = npfinaly)
+h5f.close()
--- data/kerasmodel.py
+++ data/kerasmodel.py
@@ -3,40 +3,77 @@ from keras.datasets import mnist
 from keras.utils import to_categorical, plot_model
 from keras.models import Model
 from keras.layers import Dense, Flatten, Dropout, Conv3D, MaxPooling3D, Input, BatchNormalization
+import h5py
+import numpy as np
 
 
 import os
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files (x86)\\Graphviz2.38\\bin'
-
+os.chdir('C:/Users/rajes/OneDrive/Documents/ELEC498 numba 2/ELEC-498-Capstone/data')
 
 ker = (3, 3, 3)
 pool = (2, 2, 2)
 
+
+
+h5f = h5py.File('cleandata.h5', 'r')
+b = h5f['dataset_x'][:]
+c = h5f['dataset_y'][:]
+
+
+
+
+g = to_categorical(c)
+
+for thing,stuff in zip(g,c):
+    print(thing)
+    print(stuff)
+print(min(c))
+print(max(c))
+
+
+
+#shuffle the data in unison to get a random test and training set
+rng_state = np.random.get_state()
+np.random.shuffle(b)
+np.random.set_state(rng_state)
+np.random.shuffle(g)
+
+
+
+
+#determine indeces of the training data limits
+train_amount = int((len(b)*4)/5)
+
+
+train_x, test_x = b[:train_amount,:], b[train_amount:, :]
+
+train_y, test_y = g[:train_amount,:], g[train_amount:, :]
+
+
+#following is model definition
+
 #input layer, shape of data to intake specified
-input_layer = Input((31, 31, 24, 3))
-
-
-
-
+input_layer = Input((24, 31, 31, 3))
 
 #first conv and pool
-conv_layer1 = Conv3D(filters = 23064, kernel_size = ker, activation  = 'relu', bias_initializer = 'glorot_uniform')(input_layer)
+conv_layer1 = Conv3D(filters = 32, kernel_size = ker, activation  = 'relu', bias_initializer = 'glorot_uniform')(input_layer)
 pooling_layer1 = MaxPooling3D(pool_size = pool)(conv_layer1)
 
 #second conv and pool
-conv_layer2 = Conv3D(filters = 46128, kernel_size = ker, activation = 'relu', bias_initializer = 'glorot_uniform')(pooling_layer1)
+conv_layer2 = Conv3D(filters = 64, kernel_size = ker, activation = 'relu', bias_initializer = 'glorot_uniform')(pooling_layer1)
 pooling_layer2 = MaxPooling3D(pool_size = pool)(conv_layer2)
 
 #batch normalize
-pooling_layer2 = BatchNormalization()(pooling_layer2)
+#pooling_layer2 = BatchNormalization()(pooling_layer2)
 #flatten to transition to dense
 flatten_layer = Flatten()(pooling_layer2)
 
 
 #start dense layers
-dense_layer1 = Dense(units = 2048, activation = 'tanh', bias_initializer = 'glorot_uniform')(flatten_layer)
+dense_layer1 = Dense(units = 64, activation = 'tanh', bias_initializer = 'glorot_uniform')(flatten_layer)
 dense_layer1 = Dropout(0.5)(dense_layer1)
-dense_layer2 = Dense(units = 1024, activation = 'tanh', bias_initializer = 'glorot_uniform')(dense_layer1)
+dense_layer2 = Dense(units = 32, activation = 'tanh', bias_initializer = 'glorot_uniform')(dense_layer1)
 dense_layer2 = Dropout(0.5)(dense_layer2)
 output_layer = Dense(units = 10, activation = 'softmax', bias_initializer = 'glorot_uniform')(dense_layer2)
 
@@ -47,4 +84,4 @@ model.compile(optimizer = 'adadelta', loss = 'categorical_crossentropy', metrics
 plot_model(model, to_file = "model.png")
 
 
-#model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 10)
+model.fit(train_x, train_y, validation_data = (test_x, test_y), epochs = 10)
