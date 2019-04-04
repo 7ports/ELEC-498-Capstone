557fdd0638d2fac7dd315b5bd3509b67361ffaf4
Evan <bonneauevan@gmail.com>
2019-04-04 12:38:33 -0400

    adding from VM

--- ImgFilter/filter.sh
+++ ImgFilter/filter.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-#TODO: 
 # - extract file name w/out .png extension
-# - save image as tmp.png in boopsboops.py
 #
-path=$1
-
-for i in $path*.png;
+path=$1						#path to image dir
+mkdir ~/clean					#make a directory to place clean files in
+find $1 -print0 | while IFS= read -r -d '' i	#recursive search of dir for subdir and images
 do
-	file=$(basename $i)
-	#python boopsboops.py $path$file
-	mkdir clean
-	mv tmp.png ./clean/$name-clean.png
+	if [ ! -d "$i" ]			#if not a dir
+       	then	
+		# This section extracts the filename and converts the *.jpg to tmp.png to load into
+		# filter.py which is then used to clean each image. After, the file is then named
+		# and moved to the clean directory.
+		file=$(basename $i)				#extract filename
+		echo $path$file					#User info and monitoring
+	#	convert $path$file ~/ELEC-498-Capstone/ImgFilter/run/tmp.png
+		python filter.py $path$file #~/ELEC-498-Capstone/ImgFilter/run/tmp.png
+		mv ~/ELEC-498-Capstone/ImgFilter/tmp.png ~/clean/${file%.jpg}-clean.png
+	#	rm ~/ELEC-498-Capstone/ImgFilter/run/tmp.png
+	fi	
 done
 
